b9b2443cd25d4e9be10b2219ab18d8fd429fa648
ber-bk <ber-bk@proton.me>
2025-11-26 00:50:59 +0000

    update frontend

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,12 +1,21 @@
-(function() 
+(function ()
 {
+  'use strict';
+  angular.module('ProjetoMicroApp', [])
+  .controller('ProjetoMicroController', Controller);
+  Controller.$inject = ['$scope', '$filter'];
+
+  function Controller($scope, $filter)
+  {
+    $scope.CriarMissao      = false;
+    $scope.SelecionarMissao = false;
+    $scope.ExibirMissao     = false;
 
 
 
 
 
-
-
+  };
 
 }
 )
