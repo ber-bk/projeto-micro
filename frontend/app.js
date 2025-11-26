e9da22714810aaf8152d166098d8922def9775d8
default <default@default.com>
2025-11-26 02:33:21 +0000

    update do frontend

--- frontend/app.js
+++ frontend/app.js
@@ -7,10 +7,31 @@
 
   function Controller($scope, $filter)
   {
+
     $scope.CriarMissao      = false;
     $scope.SelecionarMissao = false;
     $scope.ExibirMissao     = false;
 
+    $scope.AlterarViewCriarMissao = function ()
+    {
+      $scope.CriarMissao      = true;
+      $scope.SelecionarMissao = false;
+      $scope.ExibirMissao     = false;
+    };
+
+    $scope.AlterarViewSelecionarMissao = function ()
+    {
+      $scope.CriarMissao      = false;
+      $scope.SelecionarMissao = true;
+      $scope.ExibirMissao     = false;
+    };
+
+    $scope.AlterarViewExibirMissao = function ()
+    {
+      $scope.CriarMissao      = false;
+      $scope.SelecionarMissao = false;
+      $scope.ExibirMissao     = true;
+    };
 
 
 
